5789c5f5fd6c09ca28da6dfe3d289f40d6cb8662
rufeer <rufeer@outlook.com>
2017-03-14 22:26:54 +0800

    Update

--- spider/html_generator.py
+++ spider/html_generator.py
@@ -4,9 +4,39 @@ generate a instruction.html for every cource
 '''
 import shutil
 import os
+from bs4 import BeautifulSoup
+import re
 class HtmlGenerator(object):
-    def __init__(self, ):
-        pass
+    def __init__(self, raw, ):
+        self.chapters = []
+        
+
+
+
+
+    def distill(self):
+        soup = BeautifulSoup(self.raw, 'html.parser')
+
+        a = soup.find_all('div', class_="path")
+        data = a[0].get_text().replace('\\', ' ')
+        self.path = data.split()
+
+        self.intro = soup.find_all('p', class_="auto-wrap")[0].get_text()
+
+        for i in soup.find_all('div', class_=re.compile('chapter (chapter-active)?')):
+            name =  ' '.join(i.strong.get_text().split()[0:2])
+            videos = []
+            for j in i.find_all('a'):
+                videos.append(' '.join(j.get_text().split()[0:3]))    
+            self.chapters.append({
+                'name': name,
+                'video': videos
+            })
+
+        self.teacher_name, self.teacher_job = soup.find_all('div', class_="teacher-info")[0].get_text().split()
+        tips = soup.find_all('dd', class_="autowrap")
+        self.tip1 = tips[0].get_text()
+        self.tip2 = tips[1].get_text()
 
     def generate(self, source_html, name):
 
---
+++ test/test.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import re
+
+f = open("old/data.html", 'r') #　待处理文件
+g = open("haha.html", 'w') # 目标文件
+soup = BeautifulSoup(f.read(), 'html.parser')
+
+# # path = []
+# a = soup.find_all('div', class_="path")
+# data = a[0].get_text().replace('\\', ' ')
+# path = data.split()
+
+# jianjie = soup.find_all('p', class_="auto-wrap")[0].get_text()
+
+# chapters = []
+# for i in soup.find_all('div', class_=re.compile('chapter (chapter-active)?')):
+#     name =  ' '.join(i.strong.get_text().split()[0:2])
+#     videos = []
+#     for j in i.find_all('a'):
+#         videos.append(' '.join(j.get_text().split()[0:3]))    
+#     chapters.append({
+#         'name': name,
+#         'video': videos
+#     })
+
+teacher_name, teacher_job = soup.find_all('div', class_="teacher-info")[0].get_text().split()
+tips = soup.find_all('dd', class_="autowrap")
+tip1 = tips[0].get_text()
+tip2 = tips[1].get_text()
+
+
+f.close()
+g.close()
