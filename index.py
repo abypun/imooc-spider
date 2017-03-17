9802c6a9a069d0cab379aaf73b8edf1beab740d0
rufeer <rufeer@outlook.com>
2017-03-17 23:53:28 +0800

    Improve usability

--- index.py
+++ index.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from time import sleep
+
 from spider.course import Course
 from spider.flie_downloader import FileDownloader
 from spider.html_generator import HtmlGenerator
 
 if __name__ == '__main__':
-    # id = int(raw_input('Input your course id:'))
-    for cid in [790]:
-        course = Course(cid)
-        print "You are about to download [" + course.name + "]"
-        print "The following are the videos you will download:"
-        for video in course.videos:
-            print video['name']
-        print "---------------------------------"
+    try:
+        cid = int(raw_input('Input your course id:'))
+        if cid < 0 or cid > 1000:
+            raise ValueError
+    except:
+        print "You have a wrong ID!"
+        quit()
+    course = Course(cid)
+    print "You are about to download [" + course.name + "]"
+    print "The following are the videos you will download:"
+    for video in course.videos:
+        print video['name']
+    print "---------------------------------"
 
-        html_generator = HtmlGenerator(course.html)
-        html_generator.generate()
+    html_generator = HtmlGenerator(course.html)
+    html_generator.generate()
 
-        file_downloader = FileDownloader(course.name, course.videos)
-        if file_downloader.flag:
-            print "Mission completed"
-        sleep(60)
+    file_downloader = FileDownloader(course.name, course.videos)
+    if file_downloader.flag:
+        print "Mission completed"
+    sleep(60)
